631706dac7bdba0fc09bde5658f56380c2b46514
Saifeddine Rhouma <saifeddine.rhouma91@gmail.com>
2024-09-17 22:18:45 +0100

    fixing title home page

---
+++ dist/assets/table-no-data-DzXXEfeL.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DxrsDIwY.js";import{E as a}from"./empty-content-Der9frKm.js";import{T as e}from"./table-head-custom-w-4MLJlt.js";import{T as r}from"./TableCell-CJNt5t20.js";function i({notFound:t,sx:s}){return o.jsx(e,{children:t?o.jsx(r,{colSpan:12,children:o.jsx(a,{filled:!0,sx:{py:10,...s}})}):o.jsx(r,{colSpan:12,sx:{p:0}})})}export{i as T};
