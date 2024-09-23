038485a75d0990380ff42e5e6bb54ec43c1ab706
Saifeddine Rhouma <saifeddine.rhouma91@gmail.com>
2024-09-23 12:36:37 +0100

    fexing few issues

---
+++ dist/assets/callback-CetHMz6F.js
@@ -0,0 +1,1 @@
+import{j as a,aw as e}from"./index-C-IhHuTJ.js";function s(){return a.jsx(e,{})}export{s as default};
---
+++ dist/assets/table-no-data-CPZHThY_.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-C-IhHuTJ.js";import{E as a}from"./empty-content-CHLoRVuk.js";import{T as e}from"./table-head-custom-c1vpDJbN.js";import{T as r}from"./TableCell-YKx_Gzup.js";function i({notFound:t,sx:s}){return o.jsx(e,{children:t?o.jsx(r,{colSpan:12,children:o.jsx(a,{filled:!0,sx:{py:10,...s}})}):o.jsx(r,{colSpan:12,sx:{p:0}})})}export{i as T};
