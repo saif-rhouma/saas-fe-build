40469979a54cca90841976bf6026836981f66c0a
Saifeddine Rhouma <saifeddine.rhouma91@gmail.com>
2024-09-25 15:56:40 +0100

    fixing multiple issues

---
+++ dist/assets/callback-DUq9v2Qa.js
@@ -0,0 +1,1 @@
+import{j as a,au as e}from"./index-s58VYgz3.js";function s(){return a.jsx(e,{})}export{s as default};
---
+++ dist/assets/use-params-CEXzzucR.js
@@ -0,0 +1,1 @@
+import{dU as s,r as a}from"./index-s58VYgz3.js";function o(){const r=s();return a.useMemo(()=>r,[r])}export{o as u};
