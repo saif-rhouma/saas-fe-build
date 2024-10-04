7aefa70acf3f1bec5d9c4f7bb5715ab435334b73
Saifeddine Rhouma <saifeddine.rhouma91@gmail.com>
2024-10-04 20:35:32 +0100

    new fixess

---
+++ dist/assets/callback-DKoliwB0.js
@@ -0,0 +1,1 @@
+import{j as a,au as e}from"./index-CwDFRBrV.js";function s(){return a.jsx(e,{})}export{s as default};
---
+++ dist/assets/table-no-data-C6vJju_i.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CwDFRBrV.js";import{E as a}from"./empty-content-LLazwqBz.js";import{T as e}from"./table-head-custom-0GEy35EZ.js";import{T as r}from"./TableCell-D5WBIyLx.js";function i({notFound:t,sx:s}){return o.jsx(e,{children:t?o.jsx(r,{colSpan:12,children:o.jsx(a,{filled:!0,sx:{py:10,...s}})}):o.jsx(r,{colSpan:12,sx:{p:0}})})}export{i as T};
