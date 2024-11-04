0d9c69203b2eb212c69d54e4aead41a7121873ee
Saifeddine Rhouma <saifeddine.rhouma91@gmail.com>
2024-11-04 12:02:35 +0100

    fixing few issue

---
+++ dist/assets/table-empty-rows-BLFiUyUG.js
@@ -0,0 +1,1 @@
+import{j as l}from"./index-zyulwKWD.js";import{T as s}from"./table-head-custom-C5tz8LbQ.js";import{T as t}from"./TableCell-DS8DZ5wT.js";function i({emptyRows:r,height:o}){return r?l.jsx(s,{sx:{...o&&{height:o*r}},children:l.jsx(t,{colSpan:9})}):null}export{i as T};
---
+++ dist/assets/table-no-data-CWIOpu1y.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-zyulwKWD.js";import{E as a}from"./empty-content-jT1oSNS2.js";import{T as e}from"./table-head-custom-C5tz8LbQ.js";import{T as r}from"./TableCell-DS8DZ5wT.js";function i({notFound:t,sx:s}){return o.jsx(e,{children:t?o.jsx(r,{colSpan:12,children:o.jsx(a,{filled:!0,sx:{py:10,...s}})}):o.jsx(r,{colSpan:12,sx:{p:0}})})}export{i as T};
