953d020a373ed0414e374ab56cbd6dacbce1cd1c
Saifeddine Rhouma <saifeddine.rhouma91@gmail.com>
2024-11-05 07:45:46 +0100

    fix

---
+++ dist/assets/table-empty-rows-BLHRux3Z.js
@@ -0,0 +1,1 @@
+import{j as l}from"./index-BRWSQhLj.js";import{T as s}from"./table-head-custom-DHHtu9Bc.js";import{T as t}from"./TableCell-CZnUlaW7.js";function i({emptyRows:r,height:o}){return r?l.jsx(s,{sx:{...o&&{height:o*r}},children:l.jsx(t,{colSpan:9})}):null}export{i as T};
---
+++ dist/assets/table-no-data-DXoUwl5T.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BRWSQhLj.js";import{E as a}from"./empty-content-CoWYP2h4.js";import{T as e}from"./table-head-custom-DHHtu9Bc.js";import{T as r}from"./TableCell-CZnUlaW7.js";function i({notFound:t,sx:s}){return o.jsx(e,{children:t?o.jsx(r,{colSpan:12,children:o.jsx(a,{filled:!0,sx:{py:10,...s}})}):o.jsx(r,{colSpan:12,sx:{p:0}})})}export{i as T};
