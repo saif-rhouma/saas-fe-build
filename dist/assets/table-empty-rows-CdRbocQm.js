81f0e8b02fec98b91d66e8469bede9e25a3dc8b3
Saifeddine Rhouma <saifeddine.rhouma91@gmail.com>
2024-11-08 16:53:08 +0100

    adding few pages

---
+++ dist/assets/table-empty-rows-CdRbocQm.js
@@ -0,0 +1,1 @@
+import{j as l}from"./index-CRVB8F69.js";import{T as s}from"./table-head-custom-CJzwho5G.js";import{T as t}from"./TableCell-DefBsglL.js";function i({emptyRows:r,height:o}){return r?l.jsx(s,{sx:{...o&&{height:o*r}},children:l.jsx(t,{colSpan:9})}):null}export{i as T};
---
+++ dist/assets/table-no-data-BjqYjk_q.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CRVB8F69.js";import{E as a}from"./empty-content-C8ZIgpvp.js";import{T as e}from"./table-head-custom-CJzwho5G.js";import{T as r}from"./TableCell-DefBsglL.js";function i({notFound:t,sx:s}){return o.jsx(e,{children:t?o.jsx(r,{colSpan:12,children:o.jsx(a,{filled:!0,sx:{py:10,...s}})}):o.jsx(r,{colSpan:12,sx:{p:0}})})}export{i as T};
